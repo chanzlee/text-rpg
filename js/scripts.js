0a2e851be549863b0c4dc287ff5db01addb9f32d
Chan Lee <chanethanlee@gmail.com>
2018-08-22 16:47:17 -0700

    add hero and monster subclasses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,64 @@
-function logMessage(msg) {
+function message(msg) {
   $("#log").append("<div></div>")
   $("div").last().html(msg);
 }
 
+// set constants for consequences of attack / attacked
+var gameover = false;
+var battle = false;
+
+function Character(name, hp, str) {
+  this.name = name;
+  this.hp = hp;
+  this.str = str;
+}
+
+Character.prototype.isDead = function (){
+  this.hp <= 0 ? battle = false;
+  message(this.name + 'is dead');
+}
+
+Character.prototype.attacked = function (damage) {
+  this.hp -= damage;
+  message(this.name + 's health point became' + this.hp);
+  this.isDead();
+};
+Character.prototype.attack = function (target) {
+  message(this.name + "is attacking" + target.name);
+  target.attacked(this.att);
+};
+
+// Hero and Monster will be subclasses of character
+
+function Hero(name, hp, str, lev, xp) {
+  //use "apply" to apply Character class method on Hero
+  Character.apply(this, arguments);
+  //Syntetic Sugar: || goes down until it finds true, $$ : until false.
+  this.lev = lev || 1;
+  this.xp = xp || 0;
+}
+
+//Hero also inherits prototype methods of Character.
+Hero.prototype = Object.create(Character.prototype);
+Hero.prototype.constructor = Hero;
+
+function Monster(name, hp, str, lev, xp) {
+  Character.apply(this, arguments);
+  this.lev = lev || 1;
+  this.xp = xp || 10;
+}
+Monster.prototype = Object.create(Character.prototype);
+Monster.prototype.constructor = Monster;
+
+// Needs function to generate random monster
+
+
 $(document).ready(function(){
+  var hero = new Hero("Chan", 100, 10)
+
+  //should include something to interact with users for choosing next action.
+
+  //If user triggers "hunting monster" it should generate random monster.
+
 
 });
